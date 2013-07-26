50e0784081391b7e41b41fa5bc0b5bb05f2a1e80
Ubuntu <ubuntu@ip-172-31-14-118.us-west-2.compute.internal>
2013-07-26 05:36:24 +0000

    trying to print file

--- web.js
+++ web.js
@@ -3,7 +3,8 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+    var x  = fs.readFileSync('index.html');
+  response.send(x);
 });
 
 var port = process.env.PORT || 5000;
